edc96407dedaf21bc8a24bfa2081148a7f46e678
Matt <MVSucharski@gmail.com>
2019-02-04 16:04:10 -0500

    form working and is now populating results correctly

--- src/App.js
+++ src/App.js
@@ -9,17 +9,55 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      scotchData
+      scotchData,
+      filteredData : scotchData,
     }
+    this.change = this.change.bind(this)
+    this.filteredData = this.filteredData.bind(this)
+  }
+  change(event) {
+    let name = event.target.name
+    let value = event.target.value
+
+    this.setState({
+      [name] : value
+    }, () => {
+      console.log(this.state)
+    })
+    console.log(event.target.value)
+    this.filteredData()
   }
 
+  filteredData() {
+    
+    var newData = this.state.scotchData.filter((item) => {
+        return this.state.region
+    })
+
+    if(this.state.region !== "All"){
+      newData = newData.filter((item) => {
+        return item.region === this.state.region
+      })
+    }
+
+    if(this.state.age !== "All"){
+      newData = newData.filter((item) => {
+        return item.age === this.state.age
+      })
+    }
+
+    this.setState({
+      filteredData : newData
+      
+    })
+}
 
   render() {
     return (
       <div className="App">
         <Header />
-        <Filter />
-        <Results scotchData={this.state.scotchData} />
+        <Filter change={this.change}/>
+        <Results scotchData={this.state.filteredData} />
       </div>
     );
   }
--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -7,43 +7,43 @@ export default class Filter extends Component {
         <div className="Filter">
           <h4>Search</h4>
           <span>Region</span>
-          <select name="region" className="region">
-            <option>Any</option>
-            <option>Islay</option>
-            <option>Speyside</option>
-            <option>Highland</option>
-            <option>Lowland</option>
-            <option>Campbeltown</option>
+          <select name="region" className="region" onChange={this.props.change}>
+            <option vlaue="All">All</option>
+            <option vlaue="Islay">Islay</option>
+            <option vlaue="Speyside">Speyside</option>
+            <option vlaue="Highland">Highland</option>
+            <option vlaue="Lowland">Lowland</option>
+            <option vlaue="Campbeltown">Campbeltown</option>
             </select>
 
           <span>Age</span>  
-          <select name="age" className="age">
-            <option>Any</option>
-            <option>No Age Statement</option>
-            <option>5 Year</option>
-            <option>Between 5 and 10</option>
-            <option>10 Year</option>
-            <option>Over 10 year</option>
+          <select name="age" className="age" onChange={this.props.change}>
+            <option vlaue="All">All</option>
+            <option vlaue="No Age Statement">No Age Statement</option>
+            <option vlaue="5 Year">5 Year</option>
+            <option vlaue="Between 5 and 10">Between 5 and 10</option>
+            <option vlaue="10 Year">10 Year</option>
+            <option vlaue="Over 10 year">Over 10 year</option>
             </select>
 
             <span>Finish</span>    
-            <select name="finish" className="finish">
-            <option>Any</option>
+            <select name="finish" className="finish" onChange={this.props.change}>
+            <option vlaue="All">All</option>
             
             </select>
 
             <span>Peat</span>
-            <select name="peated" className="peated">
-            <option>Any</option>
-            <option>Yes</option>
-            <option>No</option>
+            <select name="peated" className="peated" onChange={this.props.change}>
+            <option vlaue="All">All</option>
+            <option vlaue="Yes">Yes</option>
+            <option vlaue="No">No</option>
             </select>
           
             <span>Single Malt or Blend</span>
-            <select name="malt" className="malt">
-            <option>Any</option>
-            <option>Single Malt</option>
-            <option>Blend</option>
+            <select name="malt" className="malt" onChange={this.props.change}>
+            <option vlaue="All">All</option>
+            <option vlaue="Single Malt">Single Malt</option>
+            <option vlaue="Blend">Blend</option>
             </select>
             
 
