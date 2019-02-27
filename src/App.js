dfab5a24214aa192a84da5145c440bf9f302dacd
Matt <MVSucharski@gmail.com>
2019-02-26 19:47:40 -0500

    adding scroll

--- src/App.js
+++ src/App.js
@@ -1,70 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router , Route } from 'react-router-dom';
 import Header from './Components/Header';
+import Home from './Components/Home';
+import Store from './Components/Store';
 import Filter from './Components/Filter';
 import Results from './Components/Results';
 import scotchData from './Components/data/scotchData';
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      scotchData,
-      region : "All",
-      age : "All",
-      finish : "All",
-      min_price : 0,
-      peat : "All",
-      malt : "All",
-      abv : "All",
-      name : "All",
-      filteredData : scotchData
-    }
-    this.change = this.change.bind(this)
-    this.filteredData = this.filteredData.bind(this)
-  }
-  change(event) {
-    let name = event.target.name
-    let value = event.target.value
-
-    this.setState({
-      [name]: value
-    }, () => {
-      console.log(this.state)
-      this.filteredData()
-    })
-  }
-
-  filteredData() {
-    
-    let newData = this.state.scotchData.filter((item) => {
-      return 2+2
-    })
-
-    if(this.state.region !== "All"){
-      newData = newData.filter((item) => {
-        return item.region === this.state.region
-      })
-    }
-    if(this.state.age !== "All"){
-      newData = newData.filter((item) => {
-        return item.age === this.state.age
-      })
-    }
-
-
-    this.setState(() => ({
-      filteredData : newData 
-    }))
-}
-
   render() {
     return (
+      <Router>
       <div className="App">
         <Header />
-        <Filter change={this.change} globalState={this.state}/>
-        <Results scotchData={this.state.filteredData}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/store" component={Store} />
       </div>
+      </Router>
     );
   }
 }
---
+++ src/Components/Store.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import Filter from './Filter';
+import Results from './Results';
+import scotchData from './data/scotchData';
+
+export default class Store extends Component {
+    constructor () {
+        super()
+        this.state = {
+          scotchData,
+          region : "All",
+          age : "All",
+          finish : "All",
+          min_price : 0,
+          peat : "All",
+          malt : "All",
+          abv : "All",
+          name : "All",
+          filteredData : scotchData
+        }
+        this.change = this.change.bind(this)
+        this.filteredData = this.filteredData.bind(this)
+      }
+      change(event) {
+        let name = event.target.name
+        let value = event.target.value
+    
+        this.setState({
+          [name]: value
+        }, () => {
+          console.log(this.state)
+          this.filteredData()
+        })
+      }
+    
+      filteredData() {
+        
+        let newData = this.state.scotchData.filter((item) => {
+          return 2+2
+        })
+    
+        if(this.state.region !== "All"){
+          newData = newData.filter((item) => {
+            return item.region === this.state.region
+          })
+        }
+        if(this.state.age !== "All"){
+          newData = newData.filter((item) => {
+            return item.age === this.state.age
+          })
+        }
+    
+    
+        this.setState(() => ({
+          filteredData : newData 
+        }))
+    }
+    render () {
+        return (
+            <div>
+               <Filter change={this.change} globalState={this.state}/>
+                <Results scotchData={this.state.filteredData}/>
+            </div>
+        )
+    }
+}
