eeb7cc10ba9ad3ef70f752b0e1165861682c259b
Matt <MVSucharski@gmail.com>
2019-03-10 20:20:07 -0400

    added routes to each filter

--- src/Components/History.jsx
+++ src/Components/History.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export default class History extends Component {
     render() {
         return (
-            <div className='history'>history</div>
+            <div className='history'>
+            
+            <h1>UISGEBEATHA</h1>
+
+            </div>
         )
     }
 }
